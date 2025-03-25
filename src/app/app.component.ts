e0defd0fc68c9814002e39e111b0e9f1fa247047
Marcel Angelo Navarro <marcelxangelo@gmail.com>
2025-03-25 11:01:58 +0800

    feat: update image formats to webp for performance, improve stylings, add sections, and configure TS for frontend functionality
    - Updated image formats to webp for better performance
    - Improved overall stylings for a better user experience
    - Added new sections to the portfolio
    - Configured TypeScript for enhanced frontend functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Marcel Angelo Navarro | Dev';
-  menuOpen = false; // Default state is closed
+  menuOpen = false;
 
+  // MOBILE MENU
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
+  }
+
+  // HOVER CARD
+  toggleHoverCard(event: Event): void {
+    const target = event.currentTarget as HTMLElement;
+    if (target.classList.contains('active')) {
+      target.classList.remove('active');
+    } else {
+      document.querySelectorAll('.hover-card').forEach((card) => {
+        card.classList.remove('active');
+      });
+      target.classList.add('active');
+    }
+  }
+
+  // TESTIMONIAL CARD
+  toggleTestimonialCard(event: Event) {
+    const target = event.currentTarget as HTMLElement;
+
+    document.querySelectorAll('.testimonial-card').forEach((card) => {
+      if (card !== target) {
+        card.classList.remove('active');
+      }
+    });
+
+    target.classList.toggle('active');
+  }
+
+  // FLIP CARD
+  toggleFlipCard(event: Event): void {
+    const clickedCard = event.currentTarget as HTMLElement;
+    const allCards = document.querySelectorAll('.flip-card');
+
+    allCards.forEach((card) => {
+      if (card !== clickedCard) {
+        card.classList.remove('active');
+      }
+    });
+
+    clickedCard.classList.toggle('active');
   }
 }
